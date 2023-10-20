2b0097b96b603381bc108ee43550e46c6a52540f
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-20 22:50:10 +0800

    Update TestWebsiteHTTP.sh

--- TestWebsiteHTTP.sh
+++ TestWebsiteHTTP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Seetting Up Simple Nginx Site For Internal Testing"
-sudo echo "$(hostname -I) test-site.com" | tee -a /etc/hosts
+echo "Setting Up Simple Nginx Site For Internal Testing"
+echo "$(hostname -I) test-site.com" | tee -a /etc/hosts
 cat /etc/hosts
