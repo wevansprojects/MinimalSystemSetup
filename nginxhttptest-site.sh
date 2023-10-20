dc7dafeefb8a813298500d78bccc526f8c795e1b
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-21 00:07:04 +0800

    Update nginxhttptest-site.sh

--- nginxhttptest-site.sh
+++ nginxhttptest-site.sh
@@ -6,7 +6,7 @@ sudo chown -R $USER:$USER /var/www/test-site.com/html
 echo "Create a Simple Test Site Index Page"
 echo "<html>" >> /var/www/test-site.com/html/index.html
 echo "<head>" >> /var/www/test-site.com/html/index.html
-echo "    <title>Welcome to www.terractive.com</title>" >> /var/www/test-site.com/html/index.html
+echo "    <title>Welcome to test-site.com</title>" >> /var/www/test-site.com/html/index.html
 echo "</head>" >> /var/www/test-site.com/html/index.html
 echo "<body> >> /var/www/test-site.com/html/index.html
 echo "    <h1>Success! Your Nginx server is successfully configured for <em>test-site.com</em>. </h1>" >> /var/www/test-site.com/html/index.html
