a966502295b23810c2780d81285547aa02e9a28a
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-20 19:17:43 +0800

    Create 1_DebianStarterSetup.sh

---
+++ 1_DebianStarterSetup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Updating the System"
+sudo apt update -y; sudo apt upgrade -y
+
+echo "Installing Required Packages"
+sudo apt install vim -y
+sudo apt install unattended-upgrades -y
+sudo apt install trash-cli -y
+
+echo "Applying System Service Startups"
+sudo systemctl enable unattended-upgrades
+sudo systemctl start unattended-upgrades
+
+echo "Purging Unwanted Packages"
+sudo apt purge firefox-esr* -y
+sudo apt purge nano -y
