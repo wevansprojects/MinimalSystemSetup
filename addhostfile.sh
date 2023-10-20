76350db18ec6a7176142f8941a1ba696303d4f60
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-20 22:59:50 +0800

    Update addhostfile.sh

--- addhostfile.sh
+++ addhostfile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Apending host file with test-site.com"
-sudo echo "$(hostname -I) test-site.com" | tee -a /etc/hosts
 echo "$(hostname -I) test-site.com" | tee -a /etc/hosts
 cat /etc/hosts
