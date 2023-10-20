8d3c9bbe616b1e10857a13eb77fffeea765e2f2c
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-21 00:42:25 +0800

    Update nginxhttptest-site.sh

--- nginxhttptest-site.sh
+++ nginxhttptest-site.sh
@@ -8,7 +8,7 @@ echo "<html>" >> /var/www/test-site.com/html/index.html
 echo "<head>" >> /var/www/test-site.com/html/index.html
 echo "    <title>Welcome to test-site.com</title>" >> /var/www/test-site.com/html/index.html
 echo "</head>" >> /var/www/test-site.com/html/index.html
-echo "<body> >> /var/www/test-site.com/html/index.html
+echo "<body>" >> /var/www/test-site.com/html/index.html
 echo "    <h1>Success! Your Nginx server is successfully configured for <em>test-site.com</em>. </h1>" >> /var/www/test-site.com/html/index.html
 echo "<p>This is a sample page.</p>" >> /var/www/test-site.com/html/index.html
 echo "</body>" >> /var/www/test-site.com/html/index.html
