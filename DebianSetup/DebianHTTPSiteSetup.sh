1ba1afe54bf216f01ab2ebcf4d3472a6e1a3f716
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-21 15:27:16 +0800

    Update DebianHTTPSiteSetup.sh

--- DebianSetup/DebianHTTPSiteSetup.sh
+++ DebianSetup/DebianHTTPSiteSetup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $HOME/MinimalSystemSetup/DebianSetup/HTTPServer/SetupScripts
+sudo chmod u+x *.sh
 ./1_Prerequisites.sh; ./2_bashrchelper.sh; ./3_nginxhttptestsite.sh
 echo "Http Site Setup Script Finished Testing Site"
 lynx http://www.testsite.com
