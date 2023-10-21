8ecda558c0cd5f5da982ebe899b6ec10f8b35fd4
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-21 15:05:39 +0800

    Create 2_bashrchelper.sh

---
+++ DebianSetup/HTTPServer/2_bashrchelper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd $HOME
+echo "#Nginx Test and Reload Alias Commands" >> .bashrc
+echo "alias nt='sudo nginx -t'" >> .bashrc
+echo "alias nr='sudo systemctl reload nginx.service' " >> .bashrc
+echo " " >> .bashrc
+echo "#PHP Service Reload Alias" >> .bashrc
+echo "alias phpr='sudo systemctl reload php8.2-fpm.service'" >> .bashrc
+echo " " >> .bashrc
+echo "#Trash CLI Alias (prevention of rm usage)" >> .bashrc
+echo "alias tp='sudo trash-put'" >> .bashrc
+echo "alias tl='sudo trash-list'" >> .bashrc
+echo "alias tr='sudo trash-restore'" >> .bashrc
+echo "alias te='sudo trash-empty'" >> .bashrc
+echo "alias rm='echo "try using trash-cli commands instead! example: trash-put"'" >> .bashrc
+echo 'export HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bashrc
