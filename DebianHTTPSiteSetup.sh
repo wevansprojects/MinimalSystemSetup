963a2880cacb21e00359b5ccc3dbe9faef560d0b
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-21 15:15:21 +0800

    Delete DebianHTTPSiteSetup.sh

--- DebianHTTPSiteSetup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd $HOME/MinimalSystemSetup
-./1_DebianStarterSetup.sh; ./2_bashrchelper.sh; ./3_nginxtestsite.sh
-echo "Http Site Setup Script Finished Testing Site"
-lynx http://www.testsite.com
