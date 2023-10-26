f3fb91def0508ded9e8232137a5d730f8639276a
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-27 00:16:13 +0800

    Delete DebianSetup/DebianHTTPSiteSetup.sh

--- DebianSetup/DebianHTTPSiteSetup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd $HOME/MinimalSystemSetup/DebianSetup/HTTPServer/SetupScripts
-sudo chmod u+x *.sh
-./1_Prerequisites.sh; ./2_bashrchelper.sh; ./3_nginxhttptestsite.sh
-echo "Http Site Setup Script Finished Testing Site"
-echo "To test please type: lynx http://www.testsite.com or http://testsite.com"
-exec bash
