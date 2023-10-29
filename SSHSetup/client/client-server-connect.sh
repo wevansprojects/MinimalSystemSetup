860e6233cfa12cd7fe04f3d1da605f0881221137
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-29 23:01:53 +0800

    Update client-server-connect.sh

--- SSHSetup/client/client-server-connect.sh
+++ SSHSetup/client/client-server-connect.sh
@@ -6,6 +6,8 @@ read -p ": " certname
 read -p "Choose a Port Number: " pnumber
 read -p "Type in the remote users username: " usr
 read -p "Type in server IP Address: " ip
-echo "Run the command below to connect to the server: "
+echo "Run the command below this will add the new ssh key to the remote server: "
 echo "ssh-copy-id -f -i ~/.ssh/$certname -p $pnumber $usr@$ip"
 
+echo "IMPORTANT: The Correct Way To Authenticate Is To Use This Command "
+ssh -i .ssh/$certname -p $pnumber $user@$ip
