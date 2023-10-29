4e8f18d12af7c5261189eb13ad5372c7d547eabe
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-29 12:42:29 +0800

    Update serverssh-prepubuntu.sh

--- SSHSetup/server/serverssh-prepubuntu.sh
+++ SSHSetup/server/serverssh-prepubuntu.sh
@@ -41,3 +41,5 @@ echo "Subsystem       sftp    /usr/lib/openssh/sftp-server" >> sshd_config
 
 sudo mv sshd_config /etc/ssh/sshd_config
 sudo chmod 600 /etc/ssh/sshd_config
+
+sudo systemctl restart sshd
