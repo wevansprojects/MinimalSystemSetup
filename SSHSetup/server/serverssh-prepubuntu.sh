d98f958be289811f666addfc36a5b2b5524b0470
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-29 13:39:20 +0800

    Update serverssh-prepubuntu.sh

--- SSHSetup/server/serverssh-prepubuntu.sh
+++ SSHSetup/server/serverssh-prepubuntu.sh
@@ -15,7 +15,8 @@ sudo ufw status verbose
 printf "\n"
 echo "Backup Old SSH File and Replace It"
 cd /etc/ssh
-sudo cp sshd_config sshd_config_old
+location=$(pwd)
+sudo cp $location/sshd_config sshd_config_old
 sudo rm sshd_config
 sudo cp sshd_config /etc/ssh/
 sudo chown root:root /etc/ssh/sshd_config
