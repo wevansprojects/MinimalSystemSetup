5aad4d601ddba1ff9d43a795e89e1a793b457e23
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-10-29 14:46:27 +0800

    Update serverssh-prepubuntu.sh

--- SSHSetup/server/serverssh-prepubuntu.sh
+++ SSHSetup/server/serverssh-prepubuntu.sh
@@ -6,21 +6,22 @@ printf "\n"
 echo "Adding a unique port for ssh in the firewall"
 printf "\n"
 
-sudo ufw deny ssh
-sudo ufw allow 49155
+sudo ufw allow SSH
+#sudo ufw deny ssh
+#sudo ufw allow 49155
 printf "\n"
 
-cp sshd_config $HOME
+#cp sshd_config $HOME
 
 echo "Firewall Change See Below"
 sudo ufw status verbose
-printf "\n"
-echo "Backup Old SSH File and Replace It"
-cd /etc/ssh
-sudo cp sshd_config sshd_config_old
-sudo rm sshd_config
-sudo cp $HOME/sshd_config /etc/ssh/
-sudo chown root:root /etc/ssh/sshd_config
+#printf "\n"
+#echo "Backup Old SSH File and Replace It"
+#cd /etc/ssh
+#sudo cp sshd_config sshd_config_old
+#sudo rm sshd_config
+#sudo cp $HOME/sshd_config /etc/ssh/
+#sudo chown root:root /etc/ssh/sshd_config
 sudo chmod 644 /etc/ssh/sshd_config
 
 sudo systemctl restart sshd
