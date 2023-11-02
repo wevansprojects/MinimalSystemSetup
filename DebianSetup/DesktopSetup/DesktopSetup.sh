cbd87dde425dd107df258a4285ac0e5cf72ff220
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-11-02 22:18:22 +0800

    Update DesktopSetup.sh

--- DebianSetup/DesktopSetup/DesktopSetup.sh
+++ DebianSetup/DesktopSetup/DesktopSetup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 echo "Updating the System"
+sudo rm /etc/kernel/postinst.d/z50-raspi-firmware
+sudo rm /etc/kernel/postrm.d/z50-raspi-firmware
+sudo rm /etc/initramfs/post-update.d/z50-raspi-firmware
+sudo apt purge raspi-firmware
+
+sudo apt-get autoclean
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get -f install
+
 sudo apt update -y; sudo apt upgrade -y
 
 echo "Installing Required Packages"
