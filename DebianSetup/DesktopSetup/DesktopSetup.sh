d3fdcce151220252dce3a6a26e1638c39e5a5e47
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-11-03 08:37:46 +0000

    Update DesktopSetup.sh

--- DebianSetup/DesktopSetup/DesktopSetup.sh
+++ DebianSetup/DesktopSetup/DesktopSetup.sh
@@ -39,7 +39,7 @@ sudo systemctl start sysstat
 echo "Installing Flatpak"
 sudo apt install flatpak -y 
 sudo apt install gnome-software-plugin-flatpak -y
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 echo "Installing VLC and Multimedia Codecs"
 sud apt install libavcodec-extra -y
@@ -53,9 +53,9 @@ echo "Installing Chromium"
 sudo apt install chromium
 
 echo "Adding Debian Backports"
-sudo touch /etc/apt/sources.list.d/backports.list
-echo "deb http://debian.org/debian bookworm-backports main" |sudo tee /etc/apt/sources.list.d/backports.list
-sudo apt update
+#sudo touch /etc/apt/sources.list.d/backports.list
+#echo "deb http://debian.org/debian bookworm-backports main" |sudo tee /etc/apt/sources.list.d/backports.list
+#sudo apt update
 
 echo "Setting Up Personal Bashrc"
 cd $HOME
