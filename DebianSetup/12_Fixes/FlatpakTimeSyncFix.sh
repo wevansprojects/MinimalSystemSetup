158ddd1ece85f59395ce855f61105312d9b0df26
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2023-12-27 19:00:59 +0800

    Update FlatpakTimeSyncFix.sh

--- DebianSetup/12_Fixes/FlatpakTimeSyncFix.sh
+++ DebianSetup/12_Fixes/FlatpakTimeSyncFix.sh
@@ -13,5 +13,5 @@ echo "#FallbackNTP=0.debian.pool.ntp.org 1.debian.pool.ntp.org 2.debian.pool.ntp
 echo "Change the above lines so they look like this"
 echo "NTP=time.google.com"
 echo "FallbackNTP=0.debian.pool.ntp.org 1.debian.pool.ntp.org 2.debian.pool.ntp.org 3.debian.pool.ntp.org"
-echo "Now run sudo systemctl restart systemd-timesyncd"
+echo "Save the file. Now run sudo systemctl restart systemd-timesyncd"
 sudo systemctl status systemd-timesyncd
