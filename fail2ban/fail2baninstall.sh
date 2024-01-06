21fe978082357082cdef128b567b98e1c0140f71
wevansprojects <73401706+wevansprojects@users.noreply.github.com>
2024-01-06 16:56:57 +0800

    Update fail2baninstall.sh

--- fail2ban/fail2baninstall.sh
+++ fail2ban/fail2baninstall.sh
@@ -3,14 +3,12 @@ sudo apt update
 sudo apt install fail2ban
 sudo systemctl status fail2ban.service
 cd /etc/fail2ban
-sudo cp jail.conf jail.local
-sudo cp jail.conf jail.conf
-sudo cp filter.d/nginx-badbots.conf /etc/fail2ban/filter.d/
-sudo cp filter.d/nginx-nohome.conf /etc/fail2ban/filter.d/
-sudo cp filter.d/nginx-noproxy.conf /etc/fail2ban/filter.d/
-sudo cp defaults-debian.conf /etc/fail2ban/defaults-debian.conf /etc/fail2ban/jail.d
+sudo cp $HOME/MinimalSystemSetup/fail2ban/jail.local /etc/fail2ban/jail.local
+sudo cp $HOME/MinimalSystemSetup/fail2ban/filter.d/nginx-badbots.conf /etc/fail2ban/filter.d/
+sudo cp $HOME/MinimalSystemSetup/fail2ban/filter.d/nginx-nohome.conf /etc/fail2ban/filter.d/
+sudo cp $HOME/MinimalSystemSetup/fail2ban/filter.d/nginx-noproxy.conf /etc/fail2ban/filter.d/
+sudo cp $HOME/MinimalSystemSetup/fail2ban/defaults-debian.conf /etc/fail2ban/jail.d
 sudo mkdir /var/log/nginx/
 sudo touch /var/log/nginx/error.log
 sudo touch /var/log/nginx/access.log
 sudo systemctl restart fail2ban.service
-
