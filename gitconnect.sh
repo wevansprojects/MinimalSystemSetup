fe60aa04c2bc942fea2e5e1ed69fdd22e932c41b
William Evans <williamjevans@hotmail.com>
2024-03-22 22:31:29 +0800

    gitconnect.sh amended after successful testing

--- gitconnect.sh
+++ gitconnect.sh
@@ -1,10 +1,18 @@
-read -p "Enter your github username: " username
-read -p "Enter your github emailaddress: " email
+#read -p "Enter your github username: " username
+#read -p "Enter your github emailaddress: " email
 read -p "Enter your github repository name: " repo
 git clone git@github.com:wevansprojects/$repo.git
 cd "$repo"
-git config user.name "$username"
-git config user.email "$email"
+git config user.name "William Evans"
+git config user.email "williamjevans@hotmail.com"
+git remote add origin git@github.com:wevansprojects/$repo.git
+touch testfile.txt
 git add .
 git commit -m "Initial commit"
-git push -u origin master
+git status
+git push --set-upstream origin main
+#git push -u origin master
+#git remote add origin git@github.com:wevansprojects/$repo.git
+#git add .
+#git commit -m "Initial commit"
+#git push -u origin master
