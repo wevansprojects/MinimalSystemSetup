bec89309279f07242909cf385df6d1dac86ae0e0
William Evans <73401706+wevansprojects@users.noreply.github.com>
2024-08-17 14:02:45 +0800

    Update serversshprepcentos.sh

--- SSHSetup/server/serversshprepcentos.sh
+++ SSHSetup/server/serversshprepcentos.sh
@@ -36,9 +36,9 @@ echo "Port 49155" >> sshd_config
 echo "PermitRootLogin no" >> sshd_config
 echo "AuthorizedKeysFile      %h/.ssh/authorized_keys" >> sshd_config
 echo "KbdInteractiveAuthentication no" >> sshd_config
+echo "PubKeyAuthentication yes" >> sshd_config
 echo "PasswordAuthentication yes" >> sshd_config
 echo "PermitEmptyPasswords no" >> sshd_config
-echo "PubKeyAuthentication yes" >> sshd_config
 echo "UsePAM no" >> sshd_config
 echo "X11Forwarding no" >> sshd_config
 echo "Subsystem       sftp    /usr/libexec/openssh/sftp-server" >> sshd_config
