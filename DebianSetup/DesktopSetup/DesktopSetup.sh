e1931d4b7d0f063ce6dcaaa65469cdccf231347a
William Evans <73401706+wevansprojects@users.noreply.github.com>
2026-01-29 12:56:11 +0800

    Update DesktopSetup.sh

--- DebianSetup/DesktopSetup/DesktopSetup.sh
+++ DebianSetup/DesktopSetup/DesktopSetup.sh
@@ -69,8 +69,8 @@ echo "Installing Steam"
 #flatpak install flathub com.github.tchx84.Flatseal
 
 # Core Flatpak Packages
-flatpak install flathub io.github.flattool.Warehouse -y
-flatpak install flathub com.github.tchx84.Flatseal -y
+flatpak install flathub io.github.flattool.Warehouse –system
+flatpak install flathub com.github.tchx84.Flatseal -system
 
 # Install Additional Flatpak Apps
 #flatpak install flathub io.github.martinrotter.rssguardlite -y
